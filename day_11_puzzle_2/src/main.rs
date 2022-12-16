10906c877f3723a4ddcb61d97ab52ac732df08dc
Marcin Swierczek <mswierczek90@gmail.com>
2022-12-16 10:34:04 +0100

    Add a solution for the second puzzle of day 14

--- day_11_puzzle_2/src/main.rs
+++ day_11_puzzle_2/src/main.rs
@@ -1,14 +1,65 @@
-use rug::Integer;
-use std::collections::{BTreeMap, HashMap};
+use std::collections::{BTreeMap, HashMap, HashSet, VecDeque};
 use std::error;
 use std::io::{BufRead, BufReader};
+use std::ops::{Add, Mul};
 use std::str::FromStr;
 use std::{fs::File, io};
 
+#[derive(Clone)]
+struct Composite {
+    primes: Vec<u128>,
+}
+
+impl From<u128> for Composite {
+    fn from(v: u128) -> Self {
+        let mut divisor = 2;
+        let mut dividend = v;
+        let mut primes = vec![];
+        while dividend > 1 {
+            if dividend % divisor == 0 {
+                dividend /= divisor;
+                primes.push(divisor);
+            } else {
+                if divisor >= 3 {
+                    divisor += 2;
+                } else {
+                    divisor += 1;
+                }
+            }
+        }
+        Self { primes }
+    }
+}
+
+impl Composite {
+    // assumes that a divisor is a prime number
+    fn is_divisible(&self, prime: u128) -> bool {
+        self.primes.contains(&prime)
+    }
+}
+
+impl Add for Composite {
+    type Output = Composite;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        self.primes rhs.primes
+    }
+}
+
+impl Mul for Composite {
+    type Output = Composite;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self {
+            primes: &self.primes | &rhs.primes,
+        }
+    }
+}
+
 struct Monkey {
-    items: Vec<Integer>,
-    operation: Box<dyn Fn(&Integer) -> Integer>,
-    test: Box<dyn Fn(&Integer) -> i32>,
+    items: VecDeque<Composite>,
+    operation: Box<dyn Fn(Composite) -> Composite>,
+    test: Box<dyn Fn(&Composite) -> i32>,
     inspected_counter: u64,
 }
 
@@ -18,31 +69,35 @@ impl FromStr for Monkey {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut split = s.split(";");
 
-        let items: Vec<Integer> = split.next().expect("Expected items list!")[16..]
+        let items: VecDeque<Composite> = split.next().expect("Expected items list!")[16..]
             .split(", ")
-            .map(|item| Integer::from_str(item).expect("Couldn't parse single item"))
+            .map(|item| Composite::from(item.parse::<u128>().expect("Couldn't parse single item")))
             .collect();
 
         let mut op_split = split.next().expect("Wrong operation format!")[21..].split(" ");
-        let operator: fn(&Integer, &Integer) -> Integer =
+        let operator: fn(Composite, Composite) -> Composite =
             match op_split.next().expect("Expected to get an operator!") {
-                "+" => |x: &Integer, y: &Integer| Integer::from(x + y),
-                "*" => |x: &Integer, y: &Integer| Integer::from(x * y),
+                "+" => |x: Composite, y: Composite| x + y,
+                "*" => |x: Composite, y: Composite| x * y,
                 _ => panic!("Unknown operator!"),
             };
         let operation_arg = op_split
             .next()
             .expect("Expected to get an operation argument!")
             .to_owned();
-        let operation = Box::new(move |x: &Integer| -> Integer {
+        let operation = Box::new(move |x: Composite| -> Composite {
             if operation_arg == "old" {
-                return operator(x, x);
+                return operator(x.clone(), x);
             }
-            let arg = Integer::from_str(&operation_arg).expect("Parsing the big int has failed!");
-            return operator(&x, &arg);
+            let arg = Composite::from(
+                operation_arg
+                    .parse::<u128>()
+                    .expect("Parsing the big int has failed!"),
+            );
+            return operator(x, arg);
         });
 
-        let divisor = u32::from_str(&split.next().expect("Expected to get a divisor!")[19..])
+        let divisor = u128::from_str(&split.next().expect("Expected to get a divisor!")[19..])
             .expect("Parsing the divisor has failed!");
         let true_id = split
             .next()
@@ -52,8 +107,8 @@ impl FromStr for Monkey {
             .next()
             .expect("Expected to get Monkey Id for a false case")[26..]
             .parse::<i32>()?;
-        let test = Box::new(move |x: &Integer| {
-            if x.is_divisible_u(divisor) {
+        let test = Box::new(move |x: &Composite| {
+            if x.is_divisible(divisor) {
                 return true_id;
             }
             false_id
@@ -96,21 +151,20 @@ fn main() -> io::Result<()> {
     }
 
     let monkey_ids: Vec<i32> = monkeys.keys().map(|x| x.clone()).collect();
-    for _ in 0..20 {
+    for _ in 0..10000 {
         for id in &monkey_ids {
-            let mut moved_items = HashMap::<i32, Vec<Integer>>::new();
+            let mut moved_items = HashMap::<i32, VecDeque<Composite>>::new();
             {
                 let monkey = monkeys.get_mut(id).unwrap();
-                for item in &monkey.items {
-                    let worry_level: Integer = (monkey.operation)(item) / 3;
+                while let Some(item) = monkey.items.pop_front() {
+                    let worry_level = (monkey.operation)(item);
                     let pass_to_monkey = (monkey.test)(&worry_level);
                     moved_items
                         .entry(pass_to_monkey)
                         .or_default()
-                        .push(worry_level);
+                        .push_back(worry_level);
                     monkey.inspected_counter += 1;
                 }
-                monkey.items.clear();
             }
             for (other_monkey_id, items) in moved_items.iter_mut() {
                 monkeys.entry(*other_monkey_id).and_modify(|other_monkey| {
---
+++ day_14_puzzle_1/src/main.rs
@@ -0,0 +1,219 @@
+use crate::Matter::Sand;
+use std::io::{BufRead, BufReader};
+use std::ops::{Add, Sub};
+use std::str::FromStr;
+use std::{fs::File, io};
+
+#[derive(Clone)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Point {
+    fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
+}
+
+impl Add for Point {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl Sub for Point {
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+        }
+    }
+}
+
+impl FromStr for Point {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split(",");
+        Ok(Self {
+            x: split
+                .next()
+                .expect("Missing X!")
+                .parse::<i32>()
+                .expect("Couldn't parse X"),
+            y: split
+                .next()
+                .expect("Missing Y!")
+                .parse::<i32>()
+                .expect("Couldn't parse Y"),
+        })
+    }
+}
+
+struct Path {
+    points: Vec<Point>,
+}
+
+impl Path {
+    fn get_all_rock_points(&self) -> Vec<Point> {
+        let mut rock_points = vec![];
+        for i in 0..self.points.len() - 1 {
+            let diff_point = self.points[i + 1].clone() - self.points[i].clone();
+            rock_points.push(self.points[i].clone());
+            if diff_point.x < -1 {
+                // go left
+                for j in 1..diff_point.x.abs() {
+                    rock_points.push(self.points[i].clone() - Point::new(j, 0));
+                }
+            }
+            if diff_point.x > 1 {
+                // go right
+                for j in 1..diff_point.x.abs() {
+                    rock_points.push(self.points[i].clone() + Point::new(j, 0));
+                }
+            }
+            if diff_point.y > 1 {
+                // go down
+                for j in 1..diff_point.y.abs() {
+                    rock_points.push(self.points[i].clone() + Point::new(0, j));
+                }
+            }
+            if diff_point.y < -1 {
+                // go up
+                for j in 1..diff_point.y.abs() {
+                    rock_points.push(self.points[i].clone() - Point::new(0, j));
+                }
+            }
+        }
+        rock_points.push(self.points.last().unwrap().clone());
+        rock_points
+    }
+}
+
+impl FromStr for Path {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let split = s.split(" -> ");
+        Ok(Self {
+            points: split
+                .map(|point| point.parse::<Point>().expect("Couldn't parse a Point!"))
+                .collect(),
+        })
+    }
+}
+
+#[derive(PartialEq, Clone)]
+enum Matter {
+    Rock,
+    Air,
+    Sand,
+}
+
+struct CaveScan {
+    map: Vec<Vec<Matter>>,
+    offset: usize,
+}
+
+impl CaveScan {
+    fn new(paths: Vec<Path>) -> Self {
+        let rock_points = paths.iter().map(|path| path.get_all_rock_points()).fold(
+            Vec::<Point>::new(),
+            |mut acc, mut x| {
+                acc.append(&mut x);
+                acc
+            },
+        );
+        let left_edge = rock_points
+            .iter()
+            .min_by(|&x, &y| x.x.cmp(&y.x))
+            .expect("Expected to get a left-most point")
+            .x;
+        let right_edge = rock_points
+            .iter()
+            .max_by(|&x, &y| x.x.cmp(&y.x))
+            .expect("Expected to get a right-most point")
+            .x;
+        let bottom_edge = rock_points
+            .iter()
+            .max_by(|&x, &y| x.y.cmp(&y.y))
+            .expect("Expected to get a lowest point")
+            .y;
+        let n_rows = bottom_edge as usize + 2;
+        let n_cols = (right_edge - left_edge) as usize + 3;
+
+        let offset = (left_edge - 1) as usize;
+        let mut map = vec![vec![Matter::Air; n_cols]; n_rows];
+
+        for rock in rock_points {
+            map[rock.y as usize][rock.x as usize - offset] = Matter::Rock
+        }
+
+        Self { map, offset }
+    }
+
+    fn produce_sand(&mut self) -> usize {
+        loop {
+            let mut sand = Point::new(500 - self.offset as i32, 0);
+            while (sand.y as usize) < self.map.len() - 1 {
+                // until the void hasn't been reach
+                let x = sand.x as usize;
+                let y = sand.y as usize;
+                if self.map[y + 1][x] == Matter::Air {
+                    sand = sand + Point::new(0, 1);
+                } else if self.map[y + 1][x - 1] == Matter::Air {
+                    sand = sand + Point::new(-1, 1);
+                } else if self.map[y + 1][x + 1] == Matter::Air {
+                    sand = sand + Point::new(1, 1);
+                } else {
+                    self.map[y][x] = Sand;
+                    break;
+                }
+            }
+            if (sand.y as usize) >= self.map.len() - 1 {
+                break;
+            }
+        }
+        self.map.iter().fold(0, |acc, row| {
+            acc + row
+                .iter()
+                .map(|col| if *col == Sand { 1 } else { 0 })
+                .sum::<usize>()
+        })
+    }
+
+    fn to_string(&self) -> String {
+        self.map.iter().map(|row| row.iter().map(|x| {
+            match x {
+                Matter::Rock => '#',
+                Matter::Air => '.',
+                Matter::Sand => 'o'
+            }
+        }).collect::<String>() + "\n").fold(String::new(), |acc, x| acc + &x)
+    }
+}
+
+fn main() -> io::Result<()> {
+    let input_path = std::env::args().nth(1).expect("No input file given!");
+    let file = File::open(input_path)?;
+    let reader = BufReader::new(file);
+
+    let mut cave = CaveScan::new(
+        reader
+            .lines()
+            .map(|line| Path::from_str(&line.unwrap()).unwrap())
+            .collect(),
+    );
+
+    println!("N sand units: {}", cave.produce_sand());
+
+    println!("{}", cave.to_string());
+
+    Ok(())
+}
